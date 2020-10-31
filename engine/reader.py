4bbb5caf7501c1d702af72c0880cf50c57f96697
OzoNeTT <gpoff12@mail.ru>
2020-10-31 15:56:38 +0300

    some fixes

--- engine/reader.py
+++ engine/reader.py
@@ -3,6 +3,7 @@ import lxml.html
 import json
 import re
 
+
 class invalid_password(Exception):
     def __init__(self, value): self.value = value
 
@@ -14,41 +15,43 @@ class not_valid_method(Exception):
 
     def __str__(self): return repr(self.value)
 
-class messages(object):
-    def __init__(this, login, password):
-        this.login = login
-        this.password = password
-        this.hashes = {}
-        this.auth()
 
-    def auth(this):
+class messages(object):
+    def __init__(self, login, password):
+        self.session = requests.session()
+        self.login = login
+        self.password = password
+        self.hashes = {}
+        self.auth()
+
+    def auth(self):
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/61.0.3163.100 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
             'Accept-Encoding': 'gzip, deflate',
             'Connection': 'keep-alive',
             'DNT': '1'}
-        this.session = requests.session()
-        data = this.session.get('https://vk.com/', headers=headers)
+        data = self.session.get('https://vk.com/', headers=headers)
         page = lxml.html.fromstring(data.content)
         form = page.forms[0]
-        form.fields['email'] = this.login
-        form.fields['pass'] = this.password
-        response = this.session.post(form.action, data=form.form_values())
+        form.fields['email'] = self.login
+        form.fields['pass'] = self.password
+        response = self.session.post(form.action, data=form.form_values())
         if "onLoginDone" not in response.text: raise invalid_password("Неправильный пароль!")
         return
 
-    def method(this, method, v=5.87, **params):
-        if method not in this.hashes:
-            this._get_hash(method)
+    def method(self, method, v=5.87, **params):
+        if method not in self.hashes:
+            self._get_hash(method)
         data = {'act': 'a_run_method', 'al': 1,
-                'hash': this.hashes[method],
+                'hash': self.hashes[method],
                 'method': method,
                 'param_v': v}
         for i in params:
             data["param_" + i] = params[i]
-        answer = this.session.post('https://vk.com/dev', data=data)
+        answer = self.session.post('https://vk.com/dev', data=data)
         if answer.status_code != 200:
             raise RuntimeError(f'server response with {answer.status_code}')
         return json.loads(re.findall("({.+)", answer.text)[-1])
--- engine/start.py
+++ engine/start.py
@@ -4,11 +4,11 @@ import math
 import os
 import datetime
 from time import sleep
-import local as conf
 from reader import messages
 
 VK_AUTH = [os.getenv('VK_USERNAME'), os.getenv('VK_PASSWORD')]
 VK_IGNORE_LIST = json.loads(os.getenv('VK_IGNORE_LIST'))
+
 
 def get_delay() -> float:
     now = datetime.datetime.now()
@@ -20,20 +20,23 @@ def get_delay() -> float:
 
     return (-1 / (1 + math.exp(k - 1.1 * x)) * (c_2 - c_1) + c_2) / 1000
 
+
 def get_api():
-    decoded = [base64.b64decode(s).decode('utf-8') for s in conf.VK_AUTH]
+    decoded = [base64.b64decode(s).decode('utf-8') for s in VK_AUTH]
     return messages(*decoded)
+
 
 def write_to_file(data):
     with open(f'debug.txt', 'a') as f:
         f.write(json.dumps(data) + '\n')
+
 
 def endless_reader(api):
     catch_error = 0
     e = None
     while True:
         try:
-            for id in conf.VK_IGNORE_LIST:
+            for id in VK_IGNORE_LIST:
                 resp = api.method("messages.markAsRead", peer_id=id)
                 write_to_file(resp)
                 catch_error = 0
@@ -49,6 +52,7 @@ def endless_reader(api):
             break
 
         sleep(get_delay())
+
 
 if __name__ == '__main__':
     vk_api = get_api()
